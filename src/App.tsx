95baa24963ed71c32f961c6ffcb7e53beeee2e75
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-01-15 11:03:46 -0300

    criando a rota da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import ContactList from './containers/ContactList'
-import GlobalStyle, { Container } from './styles/style'
 import store from './store'
-import Navbar from './containers/Navbar'
+
+import GlobalStyle, { Container } from './styles/style'
+import Home from './pages/Home'
+
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'novo',
+    element: <h1>Novo contato</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
       <Container>
-        <Navbar />
-        <ContactList />
+        <RouterProvider router={routers} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import ContactList from '../../containers/ContactList'
+import Navbar from '../../containers/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <ContactList />
+    </>
+  )
+}
+
+export default Home
