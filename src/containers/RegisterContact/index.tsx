c43d88fe53b91a80a2dc25ce69051e5c7a4cec9e
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-01-18 10:12:30 -0300

    feat(auth): adiciona lógica de registro de contato e navegação

--- src/containers/RegisterContact/index.tsx
+++ src/containers/RegisterContact/index.tsx
@@ -1,21 +1,62 @@
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { register as RegisterAction } from '../../store/reducers/contact'
+
 import Title from '../../components/Title'
+
 import { FormContact } from './styles'
+import { FormEvent, useState } from 'react'
 
 const RegisterContact = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const register = (ev: FormEvent) => {
+    ev.preventDefault()
+
+    dispatch(
+      RegisterAction({
+        name,
+        email,
+        phoneNumber
+      })
+    )
+    navigate('/')
+  }
+
   return (
-    <FormContact>
+    <FormContact onSubmit={register}>
       <Title TitleText="Adicionar Contato" />
       <div>
         <label htmlFor="name">Nome:</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="phoneNumber">Número de telefone:</label>
-        <input type="number" name="phoneNumber" id="phoneNumber" />
+        <input
+          type="number"
+          name="phoneNumber"
+          id="phoneNumber"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
       </div>
       <button type="submit">Adicionar</button>
       <a href="/">Voltar</a>
